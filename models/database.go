5a6a8b8a0afbdcfde9e6a68aeb2eafbba183f5ac
mamad <www.mmhmdmm83@gmail.com>
2025-02-02 22:17:20 +0330

    Feat: Create Database conneciton and test it

---
+++ models/database.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() (*gorm.DB, error) {
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+		return nil, err
+	}
+
+	DB = db
+	return db, nil
+}
+
+func TestConnection() error {
+	sqlDB, err := DB.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Ping()
+}
